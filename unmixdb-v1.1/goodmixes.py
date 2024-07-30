a0f171d18f028156782af926371b50fc8e0894d4
Diemo Schwarz <schwarz@ircam.fr>
2024-07-30 21:33:06 +0200

    also write good tracks:
    num total mixes:  2460, num good mixes with less than 4.0s of silence: 2263, num dropped mixes:   197
    num total tracks:   302,  num good tracks with less than 4.0s of silence:   281, num dropped tracks:    21

--- unmixdb-v1.1/goodmixes.py
+++ unmixdb-v1.1/goodmixes.py
@@ -7,18 +7,29 @@ import pandas as pd
 v11dir = './'
 unmixdbdir = '../../unmixdb/'
 silencethresh = 4.0 # maximum allowed duration of silence in mix
+trsilencethresh = 1.0 # maximum allowed duration of silence in track
 
-files  = pd.read_csv(v11dir + 'allmixes.csv',            sep = '\t', index_col = 0)
-chunks = pd.read_csv(v11dir + 'allmixsilencechunks.csv', sep = '\t', index_col = 0)
-badchunks = chunks.loc[chunks['duration'] > silencethresh]
+def get_good (filecsv, chunkcsv, column, silencethres):
 
-# left join to get all mixes on left, all silent chunks over threshold on right, keep only mixes without anything on right
-filterfiles = pd.merge(files, badchunks, how = 'left', on = 'mixname', indicator = 'check', left_index=False)
+    files  = pd.read_csv(filecsv,  sep = '\t', index_col = 0)
+    chunks = pd.read_csv(chunkcsv, sep = '\t', index_col = 0)
+    badchunks = chunks.loc[chunks['duration'] > silencethresh]
 
-# use indicator column from merge to select only columns for which no bad chunks existed (NULL in right half of join shows as NaN, is obfuscated by pandas)
-goodfiles = filterfiles.loc[filterfiles['check'] == 'left_only', 'mixname']
+    # left join to get all mixes on left, all silent chunks over threshold on right, keep only mixes without anything on right
+    filterfiles = pd.merge(files, badchunks, how = 'left', on = column, indicator = 'check', left_index=False)
 
-# write list of good mixes to text file
-goodfiles.to_csv(v11dir + f'unmixdb-v1.1-goodmixes-silence-less-than-{silencethresh:g}s.csv', columns = ['mixname'], header = False, index = False)
+    # use indicator column from merge to select only columns for which no bad chunks existed (NULL in right half of join shows as NaN, is obfuscated by pandas)
+    goodfiles = filterfiles.loc[filterfiles['check'] == 'left_only', column]
 
-print(f'num total mixes: {len(files)}, num good mixes with less than {silencethresh}s of silence: {len(goodfiles)}, num dropped mixes: {len(files) - len(goodfiles)}')
+    return goodfiles, files, chunks
+
+
+goodmixes,  mixes, mixchunks    = get_good(v11dir + 'allmixes.csv', v11dir + 'allmixsilencechunks.csv',    'mixname',   silencethresh)
+goodtracks, tracks, trackchunks = get_good(v11dir + 'alltracks.csv', v11dir + 'alltracksilencechunks.csv', 'trackname', trsilencethresh)
+
+# write list of good mixes and tracks to text files
+goodmixes.to_csv(v11dir + f'unmixdb-v1.1-goodmixes-silence-less-than-{silencethresh:g}s.csv', columns = ['mixname'], header = False, index = False)
+goodtracks.to_csv(v11dir + f'unmixdb-v1.1-goodtracks-silence-less-than-{silencethresh:g}s.csv', columns = ['trackname'], header = False, index = False)
+
+print(f'num total mixes:  {len(mixes):4}, num good mixes  with less than {silencethresh}s of silence: {len(goodmixes):4}, num dropped mixes:  {len(mixes) - len(goodmixes):4}')
+print(f'num total tracks: {len(tracks):4}, num good tracks with less than {silencethresh}s of silence: {len(goodtracks):4}, num dropped tracks: {len(tracks) - len(goodtracks):4}')
