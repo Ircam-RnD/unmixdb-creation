1352ac502deb34d2c21559ccb809aa4375d1774e
Diemo Schwarz <schwarz@ircam.fr>
2024-07-30 22:44:41 +0200

    write mix to track relation

--- unmixdb-v1.1/goodmixes.py
+++ unmixdb-v1.1/goodmixes.py
@@ -1,8 +1,10 @@
 
 # join 2 dataframes to find *good* mixes without too long silence
+import glob
 import re
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 v11dir = './'
 unmixdbdir = '../../unmixdb/'
@@ -33,3 +35,39 @@ goodtracks.to_csv(v11dir + f'unmixdb-v1.1-goodtracks-silence-less-than-{silencet
 
 print(f'num total mixes:  {len(mixes):4}, num good mixes  with less than {silencethresh}s of silence: {len(goodmixes):4}, num dropped mixes:  {len(mixes) - len(goodmixes):4}')
 print(f'num total tracks: {len(tracks):4}, num good tracks with less than {silencethresh}s of silence: {len(goodtracks):4}, num dropped tracks: {len(tracks) - len(goodtracks):4}')
+
+
+# load all mix label files, scan "start" with track name, write dataframe with mixname, trackname, trackpos
+
+def load_labels (mixname, prefix):
+
+    # find label file name from mp3 mixname
+    ext = re.compile('\.mp3$')
+    labelfile = ext.sub('.labels.txt', mixname) 
+
+    pre = re.compile(prefix)
+    mixbase = pre.sub('', mixname)
+
+    lab = pd.read_csv(labelfile, sep = '\t', index_col = None, header = None)
+    trstart = lab.loc[lab[3] == 'start']    # keep only lines with 'start' command
+
+    # rename 2 cols, remove others
+    trstart = trstart.rename(columns={4: 'trackname', 2: 'trackpos'}).drop(columns = [0, 1, 3])
+    trstart.insert(0, 'mixname', [ mixbase ] * len(trstart))
+    return trstart
+
+
+# problem: some labels have no mp3!!! labelfiles = glob.glob(unmixdbdir + 'mixotic-set*/mixes/*.labels.txt')
+mixfiles = glob.glob(unmixdbdir + 'mixotic-set*/mixes/*.mp3')
+
+mix_to_track = pd.DataFrame([ ], columns = ['mixname', 'trackpos', 'trackname'])
+
+for mixname in tqdm(mixfiles):
+    new = load_labels(mixname, unmixdbdir)
+    mix_to_track = pd.concat([mix_to_track, new], ignore_index=True)
+
+print(mix_to_track)
+
+mix_to_track.to_csv  (path_or_buf='mixtotrack.csv', sep='\t')
+mix_to_track.to_json (path_or_buf='mixtotrack.json', orient='records')
+
